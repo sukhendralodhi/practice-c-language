509fc0118a5f823788302ee183b6c9d2e1fc5a21
Sukhendra S Lodhi <95135906+sukhendralodhi@users.noreply.github.com>
2023-02-09 19:43:46 +0000

    use of break keyword

--- main.c
+++ main.c
@@ -61,6 +61,7 @@ int main(void) {
   */
 
   // print sum of numbers until user put zero
+  /*
   double number, sum = 0;
 
   do {
@@ -71,4 +72,19 @@ int main(void) {
   } while (number != 0.0);
 
   printf("Sum = %lf", sum);
+  */
+  // break keyword
+
+  int i;
+  double num, sum = 0.0;
+  for(i=1; i<=10; ++i) {
+    printf("Enter the value of num n%d: ",i);
+    scanf("%lf",&num);
+
+    if(num < 0.0) {
+      break;
+    }
+    sum+=num;
+  }
+  printf("Sum = %.2lf", sum);
 }
