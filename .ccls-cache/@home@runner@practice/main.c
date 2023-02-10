c449aa2108c4adaece7e0aba13ded9d6923751b0
Sukhendra S Lodhi <95135906+sukhendralodhi@users.noreply.github.com>
2023-02-10 19:28:11 +0000

    check leap year or not

--- .ccls-cache/@home@runner@practice/main.c
+++ .ccls-cache/@home@runner@practice/main.c
@@ -199,6 +199,7 @@ int main(void) {
    */
 
   // Check number is armstrong or not
+  /*
   int number, reminder, sum = 0;
   printf("Enter the number: ");
   scanf("%d", &number);
@@ -225,4 +226,21 @@ int main(void) {
   // printf("\n");
 
   // printf("Number of digits = %d", digit_counter);
+  */
+
+  // check leap year or not
+
+  int year;
+  printf("Enter the year: ");
+  scanf("%d", &year);
+
+  if (year % 400 == 0) {
+    printf("%d is leap year.\n", year);
+  } else if (year % 100 == 0) {
+    printf("%d is not leap year.\n", year);
+  } else if (year % 4 == 0) {
+    printf("%d is leap year.\n", year);
+  } else {
+    printf("%d is not leap year.\n", year);
+  }
 }
